ee397eafa61ea762bf6499f4b8ef2f139958a9a1
Parth A Patel <pap72@afsconnect1.njit.edu>
2018-02-13 14:35:42 -0500

    2nd step

--- index.php
+++ index.php
@@ -6,4 +6,13 @@
 	$year = array("2012", "396", "300","2000", "1100", "1089");
 	echo "The value of \$year: ";
 	print_r($year);
+ 
+echo "<br> <br><br>";
+ 
+    $date = date('Y/m/d', time());
+    echo "The value of \$date in Y/M/D format: ".$date."<br>";
+    
+
+ 
+ 
 ?>
