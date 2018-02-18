f9ab4ebc19bcf3b09d0e085afe7a7c13c718e229
Parth A Patel <pap72@afsconnect1.njit.edu>
2018-02-18 13:17:57 -0500

    ....

--- index.php
+++ index.php
@@ -12,17 +12,21 @@ echo "<br><br> 2. ";
     $date = date('Y/m/d', time());
     echo "The value of \$date in Y/M/D format: ".$date."<br>";
  
-echo "<br> 3. ";     
+echo "<br> 3. \$date compared to \$tar ";     
     if ($date > $tar) {
       echo "The Future"; }
     else if($date < $tar) {
       echo "The Past";
     }else{
       echo"Ooops";  }
-
+echo "<br>";
+echo "<br> 4. "; 
+    $pos = strpos($date, '/',1);
+    echo "Positions of / in \$date : " .$pos. "<br>";
+    
 echo "<br> 5. ";  
 
-  $worddate = str_word_count($date,0,"");
+  $worddate = str_word_count($date, 0 ,"/");
   echo "Number of words in \$date : " .$worddate." <br>";
   
 
@@ -35,16 +39,21 @@ echo "<br> 7. ";
 
   echo "ASCII value of \$date : ";
   echo ord($date) ;
-  
+echo "<br>";  
 echo "<br> 8. ";  
  
-  $lasttwo = substr($date, -1,0);
+  $lasttwo = substr($date, -2);
   echo "Last two words in \$date : " .$lasttwo."<br>";
   
+ echo "<br> 9. ";  
+   echo "Date Array : ";
+    print_r(explode("/",$date));
+ echo "<br>";  
+ echo "<br> 10. ";  
+ 
   
-  
-  
-  
+ 
+
   
   
   
