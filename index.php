02e411b9a9e49c1ebd6d4d9dd231eca2e29420a7
Parth A Patel <pap72@afsconnect1.njit.edu>
2018-02-20 15:00:31 -0500

    Final

--- index.php
+++ index.php
@@ -20,41 +20,59 @@ echo "<br> 3. \$date compared to \$tar : ";
       echo "The Past";
     }else{
       echo"Ooops";  }
+      
 echo "<br>";
 echo "<br> 4. "; 
-    $pos = strpos($date, '/',1);
-    echo "Positions of / in \$date : " .$pos. "<br>";
+
+    $last = 0;
+    $post = array();
+    while (($last = strpos ($date, "/", $last)) !== false) {
+     $post[] = $last;
+     $last = $last + strlen("/");
+   }
+   
+   echo "Positions of / in \$date : " ;
+   foreach ($post as $pos) {
+    echo $pos. " ";
+    }
     
+echo "<br>";      
 echo "<br> 5. ";  
 
-  $worddate = str_word_count($date, 0 ,"/");
-  echo "Number of words in \$date : " .$worddate." <br>";
+  $worddate =  explode('/', $date);
+  echo "Number of words in \$date : "; 
+  print_r(count($worddate));
   
-
-
+  
+echo "<br>"; 
 echo "<br> 6. "; 
-  $length = strlen($date);
-  echo "Length of \$date : ".$length."<br>";
 
+  $length = strlen($date);
+  echo "Length of \$date : ".$length;
+  
+echo "<br>"; 
 echo "<br> 7. ";   
 
   echo "ASCII value of \$date : ";
   echo ord($date) ;
+  
 echo "<br>";  
 echo "<br> 8. ";  
  
   $lasttwo = substr($date, -2);
-  echo "Last two words in \$date : " .$lasttwo."<br>";
+  echo "Last two words in \$date : " .$lasttwo;
   
- echo "<br> 9. ";  
+echo "<br>";    
+echo "<br> 9. ";  
+
    echo "Date Array : ";
     print_r(explode("/",$date));
+    
  echo "<br>";  
  echo "<br> 10. Leap Year <br>";  
  
 
 $datearray = explode("," , $year);
-
 
 foreach ($year as $datearray) {
   if ((($datearray % 4) == 0) && ((($datearray % 100) != 0) || (($datearray % 400) == 0))) {
@@ -64,9 +82,7 @@ foreach ($year as $datearray) {
   }
 
 }
-  
-  
-  
-  
-  
+
+    
+
 ?>
